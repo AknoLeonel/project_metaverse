0322a9e1b700b345276fd12323f8feb166167b0d
Akno Leonel <gamer.aknologia@gmail.com>
2025-09-18 16:15:20 -0300

    new feat
    head,footer, about, explore, hero

--- sections/About.jsx
+++ sections/About.jsx
@@ -16,19 +16,19 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="| About Metaversus" textStyles="text-center" />
+      <TypingText title="| Sobre a BGT" textStyles="text-center" />
 
       <motion.p
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-white">PRODUÇÃO:</span> Oferecemos todas as soluções que você precisa para planejar, executar e garantir o sucesso do seu evento esportivo, com{' '}
+        <span className="font-extrabold text-white"></span> Oferecemos todas as soluções que você precisa para planejar, executar e garantir o sucesso do seu evento esportivo, com{' '}
         <span className="font-extrabold text-white">qualidade e profissionalismo.</span>
         {' '}
-        <span className="font-extrabold text-white">SOLUÇÕES:</span> Cada evento é único. Por isso, desenvolvemos soluções personalizadas que atendem às suas necessidades específicas, proporcionando uma{' '}
+        <span className="font-extrabold text-white"></span> Cada evento é único. Por isso, desenvolvemos soluções personalizadas que atendem às suas necessidades específicas, proporcionando uma{' '}
         <span className="font-extrabold text-white">experiência memorável.</span>
         {' '}
-        <span className="font-extrabold text-white">EXCELÊNCIA:</span> Com uma equipe altamente qualificada, garantimos a excelência em cada serviço, desde a produção até a locação de materiais e consultoria especializada para eventos esportivos.
+        <span className="font-extrabold text-white"></span> Com uma equipe altamente qualificada, garantimos a excelência em cada serviço, desde a produção até a locação de materiais e consultoria especializada para eventos esportivos.
       </motion.p>
 
       <motion.img
